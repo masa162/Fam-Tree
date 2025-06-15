c5d388258d6f6fd25c39be282929543e4597402c
masa162 <belong2jazz@gmail.com>
2025-06-15 13:20:20 +0900

    rv

--- netlify/edge-functions/basic-auth.js
+++ netlify/edge-functions/basic-auth.js
@@ -1,7 +1,7 @@
 export default async (request, context) => {
   const auth = request.headers.get("authorization");
 
-  const expected = "Basic " + btoa("masa:3939");
+  const expected = "Basic " + btoa("mn:39");
   if (auth !== expected) {
     return new Response("Unauthorized", {
       status: 401,
